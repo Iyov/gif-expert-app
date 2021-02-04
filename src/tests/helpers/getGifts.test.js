dc0a172c0cfca5a083dc0740d65c1370363399a0
Francisco Barrientos <francisco.barrientos@usach.cl>
2021-02-04 16:12:36 -0300

    Se terminan las pruebas unitarias

--- src/tests/helpers/getGifts.test.js
+++ src/tests/helpers/getGifts.test.js
@@ -12,7 +12,7 @@ describe('Pruebas con getGifts Fetch', () => {
     test('Debe de traer 0 elementos', async() => {
         
         const gifs = await getGifts('');
-        console.log(gifs);
+        //console.log(gifs);
 
         expect( gifs.length ).toBe( 0 );
     });
